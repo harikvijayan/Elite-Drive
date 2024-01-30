aa6a40557ea32dd6414e08a7d85dcec2d0bf3a99
harikvijayan <harikv03@gmail.com>
2024-01-30 11:05:36 +0530

    changed port

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+            <Route></Route>
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
